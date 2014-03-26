72d7f851a984a68107dfc06a96579f0185f90a95
Amila Surendra <amilasurendra@gmail.com>
2014-03-27 04:23:49 +0530

    Activity recognition now starts with movement switch

--- src/com/example/smarttone/FullscreenActivity.java
+++ src/com/example/smarttone/FullscreenActivity.java
@@ -2,17 +2,32 @@ package com.example.smarttone;
 
 import java.util.Timer;
 
+import com.example.smarttone.ActivityUtils.REQUEST_TYPE;
 import com.example.smarttone.util.SystemUiHider;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.media.AudioManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.content.LocalBroadcastManager;
+import android.text.Spanned;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
+import android.widget.ListView;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.Switch;
 
@@ -59,6 +74,19 @@ public class FullscreenActivity extends Activity {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.activity_fullscreen);
+		
+		/*********Activity Recognition****************/
+        // Set the broadcast receiver intent filer
+        mBroadcastManager = LocalBroadcastManager.getInstance(this);
+
+        // Create a new Intent filter for the broadcast receiver
+        mBroadcastFilter = new IntentFilter(ActivityUtils.ACTION_REFRESH_STATUS_LIST);
+        mBroadcastFilter.addCategory(ActivityUtils.CATEGORY_LOCATION_SERVICES);
+
+        // Get detection requester and remover objects
+        mDetectionRequester = new DetectionRequester(this);
+        mDetectionRemover = new DetectionRemover(this);
+		
 
 		final View controlsView = findViewById(R.id.fullscreen_content_controls);
 		final View contentView = findViewById(R.id.fullscreen_content);
@@ -170,7 +198,11 @@ public class FullscreenActivity extends Activity {
 			public void onCheckedChanged(CompoundButton buttonView,
 					boolean isChecked) {
 				// TODO Auto-generated method stub
-
+				if(isChecked){
+					onStartUpdates();
+				}else{
+					onStopUpdates();
+				}
 			}
 		});
 	    
@@ -222,4 +254,223 @@ public class FullscreenActivity extends Activity {
 		mHideHandler.removeCallbacks(mHideRunnable);
 		mHideHandler.postDelayed(mHideRunnable, delayMillis);
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	/******************************** Activity Recognition  **********************************/
+	
+    private static final int MAX_LOG_SIZE = 5000;
+
+
+    // Store the current request type (ADD or REMOVE)
+    private REQUEST_TYPE mRequestType;
+
+    /*
+     *  Intent filter for incoming broadcasts from the
+     *  IntentService.
+     */
+    IntentFilter mBroadcastFilter;
+
+    // Instance of a local broadcast manager
+    private LocalBroadcastManager mBroadcastManager;
+
+    // The activity recognition update request object
+    private DetectionRequester mDetectionRequester;
+
+    // The activity recognition update removal object
+    private DetectionRemover mDetectionRemover;
+
+    /*
+     * Set main UI layout, get a handle to the ListView for logs, and create the broadcast
+     * receiver.
+     */
+
+    
+    /*
+     * Handle results returned to this Activity by other Activities started with
+     * startActivityForResult(). In particular, the method onConnectionFailed() in
+     * DetectionRemover and DetectionRequester may call startResolutionForResult() to
+     * start an Activity that handles Google Play services problems. The result of this
+     * call returns here, to onActivityResult.
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+
+        // Choose what to do based on the request code
+        switch (requestCode) {
+
+            // If the request code matches the code sent in onConnectionFailed
+            case ActivityUtils.CONNECTION_FAILURE_RESOLUTION_REQUEST :
+
+                switch (resultCode) {
+                    // If Google Play services resolved the problem
+                    case Activity.RESULT_OK:
+
+                        // If the request was to start activity recognition updates
+                        if (ActivityUtils.REQUEST_TYPE.ADD == mRequestType) {
+
+                            // Restart the process of requesting activity recognition updates
+                            mDetectionRequester.requestUpdates();
+
+                        // If the request was to remove activity recognition updates
+                        } else if (ActivityUtils.REQUEST_TYPE.REMOVE == mRequestType ){
+
+                                /*
+                                 * Restart the removal of all activity recognition updates for the 
+                                 * PendingIntent.
+                                 */
+                                mDetectionRemover.removeUpdates(
+                                    mDetectionRequester.getRequestPendingIntent());
+
+                        }
+                    break;
+
+                    // If any other result was returned by Google Play services
+                    default:
+
+                        // Report that Google Play services was unable to resolve the problem.
+                        Log.d(ActivityUtils.APPTAG, getString(R.string.no_resolution));
+                }
+
+            // If any other request code was received
+            default:
+               // Report that this Activity received an unknown requestCode
+               Log.d(ActivityUtils.APPTAG,
+                       getString(R.string.unknown_activity_request_code, requestCode));
+
+               break;
+        }
+    }
+
+    /*
+     * Register the broadcast receiver and update the log of activity updates
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Register the broadcast receiver
+        mBroadcastManager.registerReceiver(
+                updateListReceiver,
+                mBroadcastFilter);
+
+        // Load updated activity history
+        //updateActivityHistory();
+    }
+
+
+    /*
+     * Unregister the receiver during a pause
+     */
+    @Override
+    protected void onPause() {
+
+        // Stop listening to broadcasts when the Activity isn't visible.
+        mBroadcastManager.unregisterReceiver(updateListReceiver);
+
+        super.onPause();
+    }
+
+    /**
+     * Verify that Google Play services is available before making a request.
+     *
+     * @return true if Google Play services is available, otherwise false
+     */
+    private boolean servicesConnected() {
+
+        // Check that Google Play services is available
+        int resultCode =
+                GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+
+        // If Google Play services is available
+        if (ConnectionResult.SUCCESS == resultCode) {
+
+            // In debug mode, log the status
+            Log.d(ActivityUtils.APPTAG, getString(R.string.play_services_available));
+
+            // Continue
+            return true;
+
+        // Google Play services was not available for some reason
+        } else {
+
+            // Display an error dialog
+            GooglePlayServicesUtil.getErrorDialog(resultCode, this, 0).show();
+            return false;
+        }
+    }
+    /**
+     * Respond to "Start" button by requesting activity recognition
+     * updates.
+     * @param view The view that triggered this method.
+     */
+    public void onStartUpdates() {
+
+        // Check for Google Play services
+        if (!servicesConnected()) {
+
+            return;
+        }
+
+        /*
+         * Set the request type. If a connection error occurs, and Google Play services can
+         * handle it, then onActivityResult will use the request type to retry the request
+         */
+        mRequestType = ActivityUtils.REQUEST_TYPE.ADD;
+
+        // Pass the update request to the requester object
+        mDetectionRequester.requestUpdates();
+    }
+
+    /**
+     * Respond to "Stop" button by canceling updates.
+     * @param view The view that triggered this method.
+     */
+    public void onStopUpdates() {
+
+        // Check for Google Play services
+        if (!servicesConnected()) {
+
+            return;
+        }
+
+        /*
+         * Set the request type. If a connection error occurs, and Google Play services can
+         * handle it, then onActivityResult will use the request type to retry the request
+         */
+        mRequestType = ActivityUtils.REQUEST_TYPE.REMOVE;
+
+        // Pass the remove request to the remover object
+        mDetectionRemover.removeUpdates(mDetectionRequester.getRequestPendingIntent());
+
+        /*
+         * Cancel the PendingIntent. Even if the removal request fails, canceling the PendingIntent
+         * will stop the updates.
+         */
+        mDetectionRequester.getRequestPendingIntent().cancel();
+    }
+
+    
+    /**
+     * Broadcast receiver that receives activity update intents
+     * It checks to see if the ListView contains items. If it
+     * doesn't, it pulls in history.
+     * This receiver is local only. It can't read broadcast Intents from other apps.
+     */
+    BroadcastReceiver updateListReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            /*
+             * When an Intent is received from the update listener IntentService, update
+             * the displayed log.
+             */
+            //updateActivityHistory();
+        }
+    };
 }
